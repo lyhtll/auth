d5d611e2c425373ef8e3b0e23473dd1473257486
lyhtll <a62346986@gmail.com>
2025-11-12 15:22:54 +0900

    add GeneratedValue

--- src/main/kotlin/com/example/auth/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/auth/domain/user/domain/User.kt
@@ -6,6 +6,7 @@ import jakarta.persistence.*
 @Table(name = "users")
 class User(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long ?= null,
     @Column(nullable = false)
     val name: String,
